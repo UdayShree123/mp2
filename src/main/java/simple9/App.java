519a6bc154665aa0534297230e17a29d601922c5
udayshree123 <udayshree123@gmail.com>
2020-10-01 19:10:00 +0530

    1

---
+++ src/main/java/simple9/App.java
@@ -0,0 +1,14 @@
+package simple9;
+
+public class App 
+{
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      if (i == 4) {
+        break;
+      }
+      System.out.println(i);
+    }  
+  
+  }
+}
